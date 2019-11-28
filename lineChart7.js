a543221bb9a95dbe4e8cb6ae03bc9d61db032228
thejenkdog <thejenkdog@gmail.com>
2019-11-28 13:19:50 -0800

    Styling on frontend

--- lineChart7.js
+++ lineChart7.js
@@ -2,13 +2,13 @@
 
 d3.text("https://cors-anywhere.herokuapp.com/https://www.ndbc.noaa.gov/data/realtime2/FTPC1.txt", function (error, text) {
   if (error) throw error;
-  
+
   const METERSPERSECONDCONVERTMPH = 2.23694;
-  const allWindData = [];
-  let day0Data;
+  const allWindData = []; 
+  let displayData, dayMinus0, dayMinus1;
   const lines = text.split(`\n`);
 
-  for (let i = 2; i < 150; i++) {
+  for (let i = 2; i < 700; i++) {
     const line = lines[i].split(" ");
     const windSpeed = line[7];
 
@@ -29,26 +29,48 @@ d3.text("https://cors-anywhere.herokuapp.com/https://www.ndbc.noaa.gov/data/real
     const today = new Date();
     today.setHours(today.getHours() - 8);
 
-    const start = new Date(
+    const startMinus0 = new Date(
       today.getFullYear(),
       today.getMonth(),
-      today.getDate(),
+      today.getDate() - 1,
       9) // Graph x-axis always starts at 9am
 
-    const end = new Date(
+    const endMinus0 = new Date(
       today.getFullYear(),
       today.getMonth(),
-      today.getDate(),
+      today.getDate() - 1,
       18) // Graphy x-axis always finishes at 6pm
 
-    day0Data = allWindData.filter(ele => {
-      return ele.date.getTime() > start.getTime() &&
-        ele.date.getTime() < end.getTime();
+    const startMinus1 = new Date(
+      today.getFullYear(),
+      today.getMonth(),
+      today.getDate() - 2,
+      9) // Graph x-axis always starts at 9am
+
+    const endMinus1 = new Date(
+      today.getFullYear(),
+      today.getMonth(),
+      today.getDate() - 2,
+      18) // Graphy x-axis always finishes at 6pm
+
+    dayMinus0 = allWindData.filter(ele => {
+      return ele.date.getTime() > startMinus0.getTime() &&
+        ele.date.getTime() < endMinus0.getTime();
     })
+
+    dayMinus1 = allWindData.filter(ele => {
+      return ele.date.getTime() > startMinus1.getTime() &&
+        ele.date.getTime() < endMinus1.getTime();
+    })
+
+    displayData = {
+      dayMinus0,
+      dayMinus1
+    };
+
 
   }
 
-  
   let margin = { top: 10, right: 30, bottom: 48, left: 55 },
     width = 800 - margin.left - margin.right,
     height = 500 - margin.top - margin.bottom;
@@ -61,6 +83,7 @@ d3.text("https://cors-anywhere.herokuapp.com/https://www.ndbc.noaa.gov/data/real
     .attr("transform",
       "translate(" + margin.left + "," + margin.top + ")")
 
+
   let x = d3.scaleLinear()
     .domain([8, 19])
     .range([0, width])
@@ -71,7 +94,6 @@ d3.text("https://cors-anywhere.herokuapp.com/https://www.ndbc.noaa.gov/data/real
     .attr("transform", "translate(0," + height + ")")
     .call(xAxisCall)
 
-
   let y = d3.scaleLinear()
     .domain([0, 25])
     .range([height, 0])
@@ -81,26 +103,26 @@ d3.text("https://cors-anywhere.herokuapp.com/https://www.ndbc.noaa.gov/data/real
     .attr("class", "y-axis")
     .call(yAxisCall);
 
-
-  svg.append("path")
-    .datum(day0Data)
-    .attr("fill", "none")
-    .attr("stroke", "#4287f5")
-    .attr("stroke-width", 1.5)
-    .attr("d", d3.line()
-      .x((d) => { return x(d.hourValue) })
-      .y((d) => { return y(d.value) })
-    )
-
-  svg.append("g")
-    .selectAll("dot")
-    .data(day0Data)
+  let line = svg
+    .append('g')
+    .append("path")
+      .datum(displayData.dayMinus0)
+      .attr("d", d3.line()
+        .x((d) => { return x(d.hourValue) })
+        .y((d) => { return y(d.value) }))
+      .attr("stroke", "#4287f5")
+      .style("fill", "none")
+      .style("stroke-width", 3)
+  
+  let dot = svg
+    .selectAll("circle")
+    .data(displayData.dayMinus0)
     .enter()
     .append("circle")
-    .attr("cx", d => { return x(d.hourValue) })
-    .attr("cy", d => { return y(d.value) })
-    .attr("r", 4)
-    .attr("fill", "#4287f5")
+      .attr("cx", d => { return x(d.hourValue) })
+      .attr("cy", d => { return y(d.value) })
+      .attr("r", 4)
+      .style("fill", "#4287f5")
 
   svg.append("text")
     .attr("transform",
@@ -116,5 +138,42 @@ d3.text("https://cors-anywhere.herokuapp.com/https://www.ndbc.noaa.gov/data/real
     .attr("dy", "1em")
     .style("text-anchor", "middle")
     .text("speed in miles per hour (mph)");
+
+  let allData = ["dayMinus0", "dayMinus1"];
+
+  d3.select("#selectButton")
+    .selectAll('myOptions')
+      .data(allData)
+    .enter()
+      .append('option')
+    .text(function (d) {return d; })
+    .attr("value", function (d) { return d; })
+
+  function update(selectedData) {
+
+    let dataFilter = displayData[selectedData];
+
+    line
+      .datum(dataFilter)
+      .transition()
+      .duration(1000)
+      .attr("d", d3.line()
+        .x(function (d) { return x(d.hourValue) })
+        .y(function (d) { return y(d.value) })
+      )
+  
+    dot
+      .data(dataFilter)
+      .transition()
+      .duration(1000)
+        .attr("cx", d => { return x(d.hourValue) })
+        .attr("cy", d => { return y(d.value) })
+
+  }
+
+  d3.select("#selectButton").on("change", function(d) {
+    let selectedOption = d3.select(this).property("value")
+    update(selectedOption)
+  })
 
 });
