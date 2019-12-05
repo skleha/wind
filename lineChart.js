fc9703ce84d700b4bf5c2955f49105a96de5110d
thejenkdog <thejenkdog@gmail.com>
2019-12-04 17:40:56 -0800

    Uncommented API call

--- lineChart.js
+++ lineChart.js
@@ -178,7 +178,7 @@ function getDateString(displayName) {
 }
 
 
-// d3.text("https://cors-anywhere.herokuapp.com/https://www.ndbc.noaa.gov/data/realtime2/46026.txt", function (error, text) {
+d3.text("https://cors-anywhere.herokuapp.com/https://www.ndbc.noaa.gov/data/realtime2/46026.txt", function (error, text) {
   if (error) throw error;
 
   const allWindData = parseAllWindData(text);
