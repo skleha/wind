ea43fd40773781278994be70c39feec37e252c30
thejenkdog <thejenkdog@gmail.com>
2019-12-09 14:44:24 -0800

    Add modal, modal buttons, image

---
+++ utility.js
@@ -0,0 +1,29 @@
+
+function openModal() {
+  let openModalButton = document.getElementById("about-button");
+  let modal = document.getElementById('modal');
+  let modalChild = document.getElementById('modal-child');
+  
+  openModalButton.addEventListener('click', () => {
+    modal.classList.toggle('open');
+    modalChild.classList.toggle('open');
+  })
+}
+
+function closeModal() {
+  let closeModalButton = document.getElementById("close-button")
+  let modal = document.getElementById('modal');
+  let modalChild = document.getElementById('modal-child');
+
+  closeModalButton.addEventListener('click', () => {
+    modal.classList.toggle('open');
+    modalChild.classList.toggle('open');
+  })
+}
+
+function fireWhenReady(func) {
+  document.addEventListener('DOMContentLoaded', func);
+}
+
+fireWhenReady(openModal);
+fireWhenReady(closeModal);
